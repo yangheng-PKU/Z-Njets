2ed1835858af895f5b19584f21a5f9601294b1b2
Heng-Yang <55174411+Heng-Yang@users.noreply.github.com>
2020-05-20 12:19:51 +0800

    Add files via upload

--- python/postprocessing/examples/exampleModule.py
+++ python/postprocessing/examples/exampleModule.py
@@ -22,39 +22,42 @@ class exampleProducer(Module):
         self.out.branch("z_pt","F");
 	self.out.branch("z_phi","F");
 	self.out.branch("ngoodjets","I");
-	self.out.branch("jet_id","I",lenVar="ngoodjets");
+        self.out.branch("ngoodmuons","I");
+        self.out.branch("muon_pt","F",lenVar="ngoodmuons");
+        self.out.branch("muon_eta","F",lenVar="ngoodmuons");
+	#self.out.branch("jet_id","I",lenVar="ngoodjets");
 	self.out.branch("jet_pt","F",lenVar="ngoodjets");
 	self.out.branch("jet_phi","F",lenVar="ngoodjets");
-	self.out.branch("dphi_zjet","F",lenVar="ngoodjets");
+	#self.out.branch("dphi_zjet","F",lenVar="ngoodjets");
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
-        if not (event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ or event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ or event.HLT_IsoTkMu24 or event.HLT_IsoMu24):
+        '''
+	if not (event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ or event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ or event.HLT_IsoTkMu24 or event.HLT_IsoMu24):
 	    self.out.fillBranch("pass_selection",0)
             return True
         '''
-	if hasattr(event,"Generator_weight"):
-	    self.out.fillBranch("gen_weight",event.Generator_weight)
-	else:
-            self.out.fillBranch("gen_weight",0)
-	'''
 	electrons = Collection(event, "Electron")
         muons = Collection(event, "Muon")
         jets = Collection(event, "Jet")
         Z = ROOT.TLorentzVector()
 
 	tight_muons = []
-	
+	goodmuons_pt = []
+        goodmuons_eta = [] 
+
 	if (len(muons)<=1):
 		self.out.fillBranch("pass_selection",0)
                 return True
 	for i in range(0,len(muons)):
-            if (muons[i].eta) < 2.4 and (muons[i].mediumId) and (muons[i].pfIsoId)>=3:
+            #if (muons[i].eta) < 2.4 and (muons[i].mediumId) and (muons[i].pfIsoId)>=3:
+	     if (muons[i].eta) < 2.4 and (muons[i].mediumId):
 	        if (muons[i].pt) <= 25:
                     continue
 		for j in range(i+1,len(muons)):
-  		    if (muons[j].eta) < 2.4 and (muons[j].mediumId) and (muons[j].pfIsoId)>=3:
+  		    #if (muons[j].eta) < 2.4 and (muons[j].mediumId) and (muons[j].pfIsoId)>=3:
+	            if (muons[j].eta) < 2.4 and (muons[j].mediumId):
 	                if (muons[j].pt) <= 20:
 			    continue
 		        if (muons[i].charge + muons[j].charge == 0):
@@ -64,16 +67,26 @@ class exampleProducer(Module):
 	            		self.out.fillBranch("z_pt",Z.Pt())
 				self.out.fillBranch("z_mass",Z.M())
 				self.out.fillBranch("z_phi",Z.Phi())
-				tight_muons.append(i)            
-				tight_muons.append(j)
-				#return True
+				tight_muons.append(muons[i])            
+				tight_muons.append(muons[j])
 	
-        #print(2)
 	if len(tight_muons) < 2:
 	    self.out.fillBranch("pass_selection",0)
 	    return True
-	#print(3)
 
+        ngoodmuons = 0
+        ngoodmuons = len(tight_muons)
+	if ngoodmuons != 2:
+            print(ngoodmuons)
+
+        goodmuons_pt.append(tight_muons[0].pt)
+        goodmuons_pt.append(tight_muons[1].pt)
+        goodmuons_eta.append(tight_muons[0].eta)
+        goodmuons_eta.append(tight_muons[1].eta)        
+        
+        self.out.fillBranch("muon_pt",goodmuons_pt)
+        self.out.fillBranch("muon_eta",goodmuons_eta)        
+        
 	ngoodjets = 0
         goodjets_pt = []
 	goodjets_id = []
@@ -88,13 +101,16 @@ class exampleProducer(Module):
 	    if jets[k].pt < 30:
 		continue
 	    #print(6)
+	    '''
 	    if not (jets[k].jetId & 1):
 		continue
 	    #print(7)
+	    '''
 	    pass_lepton_dr_cut = True
 
 	    for i in range(0,len(tight_muons)):
-		if deltaR(muons[tight_muons[i]].eta,muons[tight_muons[i]].phi,jets[k].eta,jets[k].phi) < 0.4:
+		#if deltaR(muons[tight_muons[i]].eta,muons[tight_muons[i]].phi,jets[k].eta,jets[k].phi) < 0.4:
+                if deltaR(tight_muons[i].eta,tight_muons[i].phi,jets[k].eta,jets[k].phi) < 0.4:
 	            pass_lepton_dr_cut = False
 
 	    if not pass_lepton_dr_cut:
@@ -102,18 +118,17 @@ class exampleProducer(Module):
 
             ngoodjets += 1
             goodjets_pt.append(jets[k].pt)
-	    goodjets_id.append(jets[k].jetId)
+	    #goodjets_id.append(jets[k].jetId)
 	    goodjets_phi.append(jets[k].phi)	    
-	    goodjets_dphi_zjet.append(deltaPhi(Z.Phi(),jets[k].phi))            
+	    #goodjets_dphi_zjet.append(deltaPhi(Z.Phi(),jets[k].phi))            
 
         if ngoodjets != len(goodjets_pt):
             print(error)
 
-	self.out.fillBranch("ngoodjets",ngoodjets)
         self.out.fillBranch("jet_pt",goodjets_pt)
-	self.out.fillBranch("jet_id",goodjets_id)
+	#self.out.fillBranch("jet_id",goodjets_id)
 	self.out.fillBranch("jet_phi",goodjets_phi)
-	self.out.fillBranch("dphi_zjet",goodjets_dphi_zjet)
+	#self.out.fillBranch("dphi_zjet",goodjets_dphi_zjet)
 	'''
 	if(njet!=0):
 	    print(njet)
